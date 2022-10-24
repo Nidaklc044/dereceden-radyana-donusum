5d59ac768fe2225ef90ba608abd282ab3d7bdabe
Nidaklc044 <116462585+Nidaklc044@users.noreply.github.com>
2022-10-24 11:18:00 +0300

    dereceden radyana donusum
    c#

---
+++ dereceradyandonusumleri/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Derece degerini giriniz");
+int derece=Convert.ToInt32(Console.ReadLine());
+double radyan = derece * Math.PI / 180;
+double gradyan = derece * 200 / 180;
+Console.WriteLine("derecenın radyan degeri=" + radyan);
+Console.WriteLine("derecenin gradyan degeri="+gradyan);
+Console.ReadKey();
